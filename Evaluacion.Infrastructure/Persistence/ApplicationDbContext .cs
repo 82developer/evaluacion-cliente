cef5d94137c58053c5ec7f6c8000add4c0a318ad
Jose Alvaro Del Castilllo Aquino <alvarodelcastillo@outlook.com>
2025-10-10 17:48:42 -0500

    chore: add db context

---
+++ Evaluacion.Infrastructure/Persistence/ApplicationDbContext .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        //    : base(options) { }
+
+        //// Example DbSets
+        ////public DbSet<User> Users => Set<User>();
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    // Fluent API mappings here
+        //}
+    }
+}
