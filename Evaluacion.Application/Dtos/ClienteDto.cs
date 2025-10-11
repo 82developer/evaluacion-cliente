af190ca743b9c3c697b588cfab771f52caeb389d
Jose Alvaro Del Castilllo Aquino <alvarodelcastillo@outlook.com>
2025-10-10 20:30:45 -0500

    chore: add properties in clientDto

--- Evaluacion.Application/Dtos/ClienteDto.cs
+++ Evaluacion.Application/Dtos/ClienteDto.cs
@@ -8,5 +8,11 @@ namespace Evaluacion.Application.Dtos
 {
     public class ClienteDto
     {
+        public int Id { get; set; }
+        public string Ruc { get; set; }
+        public string RazonSocial { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
     }
 }
