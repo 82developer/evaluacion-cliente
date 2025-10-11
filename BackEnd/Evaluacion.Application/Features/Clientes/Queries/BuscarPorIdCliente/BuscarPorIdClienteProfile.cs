f82cd894c7549d52e23991cdc132e1e98b5adcab
Jose Alvaro Del Castilllo Aquino <alvarodelcastillo@outlook.com>
2025-10-10 22:47:16 -0500

    chore: relocation

---
+++ BackEnd/Evaluacion.Application/Features/Clientes/Queries/BuscarPorIdCliente/BuscarPorIdClienteProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion.Application.Features.Clientes.Queries.BuscarPorIdCliente
+{
+    public class BuscarPorIdClienteProfile:Profile
+    {
+        public BuscarPorIdClienteProfile()
+        {
+           CreateMap<Domain.Cliente, Dtos.ClienteDto>(); 
+        }
+    }
+}
